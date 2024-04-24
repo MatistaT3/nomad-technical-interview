ce6a6720636c052664bac1c1ffb946198cfb07e2
José Sáez <jsaez@Joses-MacBook-Pro.local>
2024-04-23 20:29:04 -0400

    Adds full server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,25 @@
-import express from 'express';
-import cartRoutes from './routes/cart.routes';
-import cors from 'cors';
+import express from "express";
+import cartRoutes from "./routes/cart.routes";
+import cors from "cors";
+import path from "path";
 
 const app = express();
 const PORT = 4000;
 
 app.use(express.json());
-
+app.use(
+  "/",
+  express.static(path.join(__dirname, "public"), { extensions: ["html"] })
+);
+app.use("/_next", express.static(path.join(__dirname, "public/_next")));
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "http://localhost:4000",
+      "http://localhost:4001",
+    ],
   })
 );
 
